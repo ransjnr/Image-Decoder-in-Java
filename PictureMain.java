f7d75f107ecdf9844ebd14e54cd1bf9206f31549
Ransford Oppong <faradayfoundationgh@gmail.com>
2024-02-12 09:47:25 -0800

    renaming

--- PictureMain.java
+++ PictureMain.java
@@ -1,14 +1,18 @@
+//import input output class
+import java.io.*;
 
 //class for image decode
 class PicturePlotter{
-
+    int width;
+    int height;
 
     //a constructor that takes the width and height of the image as parameters
     public PicturePlotter(int width, int height){
-
+        this.width = width;
+        this.height = height;
     }
     //a method that loop through each line. passing the count and color values
-    public void (int x, int y, int count, int color){
+    public void plot(int x, int y, int count, int color){
 
     }
 }
@@ -16,7 +20,8 @@ class PicturePlotter{
 public class PictureMain{
     
     public static void main(String[] args){
+        //scanning the txt file into project
+        File picture = new File("picture.txt");
 
-        
     }
 }
