bfdf9723cf0eb2e5fae5aeacd8a118f9a1a3c7f3
Ransford Oppong <faradayfoundationgh@gmail.com>
2024-02-11 22:23:29 -0800

    main function created

--- PictureMain.java
+++ PictureMain.java
@@ -1,6 +1,8 @@
 
 //class for image decode
 class PicturePlotter{
+
+
     //a constructor that takes the width and height of the image as parameters
     public PicturePlotter(int width, int height){
 
@@ -8,5 +10,13 @@ class PicturePlotter{
     //a method that loop through each line. passing the count and color values
     public void (int x, int y, int count, int color){
 
+    }
+}
+
+public class PictureMain{
+    
+    public static void main(String[] args){
+
+        
     }
 }
