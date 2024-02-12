8260467fb3c11058f689f1bac23d00214404ccab
Ransford Oppong <faradayfoundationgh@gmail.com>
2024-02-11 22:12:00 -0800

    main file PcitureMain.java created

---
+++ PictureMain.java
@@ -0,0 +1,12 @@
+
+//class for image decode
+class PicturePlotter{
+    //a constructor that takes the width and height of the image as parameters
+    public PicturePlotter(int width, int height){
+
+    }
+    //a method that loop through each line. passing the count and color values
+    public void (int x, int y, int count, int color){
+
+    }
+}
