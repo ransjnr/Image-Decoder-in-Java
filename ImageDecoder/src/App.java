ece89268272ecbc1d3b2d13dad37ecde88ef3bd6
Ransford Oppong <faradayfoundationgh@gmail.com>
2024-02-13 04:32:21 -0800

    update

--- ImageDecoder/src/App.java
+++ ImageDecoder/src/App.java
@@ -8,8 +8,7 @@ class PicturePlotter{
 
     //a constructor that takes the width and height of the image as parameters
     public PicturePlotter(int width, int height){
-        this.width = width;
-        this.height = height;
+
     }
     //a method that loop through each line. passing the count and color values
     public void plot(int x, int y, int count, int color){
@@ -19,7 +18,8 @@ class PicturePlotter{
 
 public class PictureMain{
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception
+    {
         //scanning the txt file into project
         File picture = new File("picture.txt");
 
